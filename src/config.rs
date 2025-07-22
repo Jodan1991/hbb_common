f787db7250a11b8d0bbaa98ec704c93c64a1078c
shi <shi@castech.local>
2025-07-22 14:52:21 +0800

    修改本地的中继服务器

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["192.168.0.47"];
+pub const RS_PUB_KEY: &str = "7wpYJEAefpCpZwHHbhIAS4LT7JpalbSJYu3NM7vKAsU=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
